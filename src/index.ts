90ccde0cfd5f3e22c9513b4c658c1574aaf8cc6f
minhyeong <minhyeong.dev@gmail.com>
2025-07-17 04:09:24 +0900

    fix: version in object

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,13 @@ import instance from "./instance";
 import { Http, Option } from "./interface";
 
 type Handler<T> = {
-  [p in keyof T]: (
-    version?: string,
-    option?: Omit<Option, "version">
-  ) => Http;
+  [p in keyof T]: (option?: Option) => Http;
 };
 
 function handler<T extends Record<string, string>>(obj: T, globalOption?: Option): Handler<T> {
   return Object.keys(obj).reduce<Handler<T>>((acc, cur) => {
-    acc[cur as keyof T] = (
-      version?: string,
-      option?: Omit<Option, "version">
-    ) => instance(obj[cur], { version, ...globalOption, ...option });
+    acc[cur as keyof T] = (option?: Option) => 
+      instance(obj[cur], { ...globalOption, ...option });
     return acc;
   }, {} as Handler<T>);
 }
