200b93bab03c9f6fb5fa8f0ecbf3a3bbbea377bb
minhyeong <minhyeong.dev@gmail.com>
2025-09-05 04:51:45 +0900

    feat: create ```CookieConfig```

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,17 @@
+// 전역 쿠키 저장소
 let globalCookies = "";
 
-// 쿠키 설정 함수 (React/Express에서 호출)
-export function setCookies(cookies: string) {
-  globalCookies = cookies;
+class CookieConfig {
+  constructor(cookies: string) {
+    globalCookies = cookies;
+  }
 }
 
-// 쿠키 가져오기 함수
 export function getCookies(): string {
-  // React 환경에서는 document.cookie 사용
-  if (typeof document !== "undefined") {
+  if (typeof document !== 'undefined') {
     return document.cookie;
   }
-  // Express 환경에서는 전역 저장소 사용
   return globalCookies;
 }
+
+export { CookieConfig };
