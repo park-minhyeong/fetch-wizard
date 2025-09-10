9bea5b7869ec608f0708c53cc9433e8ce1a129e4
minhyeong <minhyeong.dev@gmail.com>
2025-09-11 08:00:05 +0900

    fix: set interface to create and update

--- src/_test/src/interfaces/Todo.ts
+++ src/_test/src/interfaces/Todo.ts
@@ -4,3 +4,7 @@ export interface Todo{
 	title: string,
 	completed: boolean
 }
+const todoAutoSetKeys=['id'] as const;
+type TodoAutoSetKey=typeof todoAutoSetKeys[number];
+export interface TodoCreate extends Omit<Todo, TodoAutoSetKey>{}
+export interface TodoUpdate extends Partial<TodoCreate>{}
