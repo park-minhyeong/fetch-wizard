a32772850f8ba34ee6299a44b0a0c234b532ee10
minhyeong <minhyeong.dev@gmail.com>
2025-09-11 06:53:16 +0900

    chore: vite config

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  root: './src/_test',
+  esbuild: {
+    jsx: 'automatic'
+  }
+})
